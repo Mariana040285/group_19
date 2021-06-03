4651f1503343cc0e77c53ac1caaa989f840b5ea8
Мариана040285 <m.v.polyanna@gmail.com>
2021-06-03 14:30:49 +0300

    first commit

---
+++ script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+mkdir Sport
+cd Sport
+mkdir Colours Weather Flowers
+cd Weather
+touch books.txt picture.txt poster.txt Helicopter.gson Plane.json
+mkdir House Kitchen Bedroom
+ls 
+mv books.txt picture.txt House
